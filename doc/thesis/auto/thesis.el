041f4a8a91ffad624787a469bfb83b517f042360
Qiyuan Pu <pqy7172@gmail.com>
2017-12-29 12:45:39 +0800

    nothing

---
+++ doc/thesis/auto/thesis.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "thesis"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "swfcthesis"
+    "swfcthesis10"
+    "todonotes")
+   (TeX-add-symbols
+    '("inlineitem" ["argument"] 0))
+   (LaTeX-add-labels
+    "sec:background"
+    "sec:devel-envir"
+    "sec:tools"
+    "sec:install"
+    "sec:qemu"
+    "sec:wine"
+    "sec:debian-i386-support"
+    "cha:leading-knowledge"
+    "sec:instruction-set"
+    "sec:register"
+    "sec:top-level-design"
+    "sec:detailed-design"
+    "sec:boot-loader"
+    "fig:working-flow-boot-loader"
+    "sec:32-bit-mode"
+    "sec:chose-disk"
+    "sec:struct-floppy-disk"
+    "fig:flpy1.png"
+    "sec:flowch-boot-load"
+    "fig:flowchart-of-boot-loader"
+    "sec:running-result"
+    "fig:iplRes"
+    "sec:dis-boo-inf"
+    "sec:rea-sec-sec"
+    "sec:rea-two-sid"
+    "sec:the-nex-cyl")
+   (LaTeX-add-bibliographies))
+ :latex)
+
