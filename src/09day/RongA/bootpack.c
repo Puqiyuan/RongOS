0d882853f60f5edcc5099095ccbf80ff8351288e
Qiyuan Pu <pqy7172@gmail.com>
2018-01-11 17:40:07 +0800

    arrange source file

--- src/09day/RongA/bootpack.c
+++ src/09day/RongA/bootpack.c
@@ -1,17 +1,6 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-struct MOUSE_DEC
-{
-	unsigned char buf[3], phase;
-	int x, y, btn;
-};
-
-extern struct FIFO8 keyfifo, mousefifo;
-void enable_mouse(struct MOUSE_DEC *mdec);
-void init_keyboard(void);
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
-
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
@@ -97,99 +86,4 @@ void HariMain(void)
 						}
 				}
 		}
-}
-
-
-
-#define PORT_KEYDAT 0x0060
-#define PORT_KEYSTA 0x0064
-#define PORT_KEYCMD 0x0064
-#define KEYSTA_SEND_NOTREADY 0x02
-#define KEYCMD_WRITE_MODE 0x60
-#define KBC_MODE 0x47
-
-void wait_KBC_sendready(void) // wait for the keyboard controller to be able to transmit data
-{
-	for (;;)
-		{
-			{
-				if ( (io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0 )
-					break;
-			}
-		}
-
-	return;
-}
-
-
-void init_keyboard(void) // initializtion of keyboard controller
-{
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, KBC_MODE);
-
-	return;
-}
-
-#define KEYCMD_SENDTO_MOUSE 0xd4
-#define MOUSECMD_ENABLE 0xf4
-
-void enable_mouse(struct MOUSE_DEC *mdec)
-{
-	// mouse effective
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	return; // if it works, ACK (0xfa) will be sent
-}
-
-
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
-{
-	if (mdec -> phase == 0)// waiting for mouse 0xfa
-		{
-			if (dat == 0xfa)
-				mdec -> phase = 1;
-			return 0;
-		}
-
-	if (mdec -> phase == 1) // waiting for the 1st byte of the mouse
-		{
-			if ((dat & 0xc8) == 0x08)// it was the correct first byte.
-				{
-					mdec -> buf[0] = dat;
-					mdec -> phase = 2;
-				}
-			return 0;
-		}
-
-	if (mdec -> phase == 2) // waiting for the second byte of the mouse
-		{
-			mdec -> buf[1] = dat;
-			mdec -> phase = 3;
-			return 0;
-		}
-
-	if (mdec -> phase == 3)// waiting for the third byte of the mouse
-		{
-			mdec -> buf[2] = dat;
-			mdec -> phase = 1;
-			mdec -> btn = mdec -> buf[0] & 0x07;
-			mdec -> x = mdec -> buf[1];
-			mdec -> y = mdec -> buf[2];
-
-			if ((mdec -> buf[0] & 0x10) != 0)
-				mdec -> x |= 0xffffff00;
-
-			if ((mdec -> buf[0] & 0x20) != 0)
-				mdec -> y |= 0xffffff00;
-
-			mdec -> y = - mdec -> y; // in the mouse, the sign in the y direction is the opposite of the screen.
-			
-			return 1;
-		}
-
-	return -1; // there should not be anything to come here.
 }
--- src/09day/RongA/int.c
+++ src/09day/RongA/int.c
@@ -24,34 +24,6 @@ void init_pic(void)
 	return;
 }
 
-#define PORT_KEYDAT 0x0060
-
-struct FIFO8 keyfifo;
-
-void inthandler21(int *esp)
-{
-	unsigned char data;
-	io_out8(PIC0_OCW2, 0x61);
-	
-	data = io_in8(PORT_KEYDAT);
-	fifo8_put(&keyfifo, data);
-	
-	return;
-}
-
-struct FIFO8 mousefifo;
-
-void inthandler2c(int *esp) // interruption from PS/2 mouse
-{
-	unsigned char data;
-	io_out8(PIC1_OCW2, 0x64); // notify PIC1 of IRQ-12 reception completion.
-	io_out8(PIC0_OCW2, 0x62); // notify PIC0 of IRQ-02 reception completion.
-	data = io_in8(PORT_KEYDAT);
-	fifo8_put(&mousefifo, data);
-	return;
-}
-
-
 void inthandler27(int *esp)
 {
 	io_out8(PIC0_OCW2, 0x67);
