c39671f7ab34acd8a2c35d9167a797707eed1062
Qiyuan Pu <pqy7172@gmail.com>
2018-03-23 12:34:30 +0800

    finish app program

---
+++ src/20day/RongD/hlt.asm
@@ -0,0 +1,12 @@
+		[bits 32]
+		mov al, 'h'
+		call 2*8:0xbe8
+		mov al, 'e'
+		call 2*8:0xbe8
+		mov al, 'l'
+		call 2*8:0xbe8
+		mov al, 'l'
+		call 2*8:0xbe8
+		mov al, 'o'
+		call 2*8:0xbe8
+		retf
